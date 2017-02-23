21217cd8077cb1e202a1641c297660ad041eb5f7
unknown <>
2017-02-23 12:34:03 +0800

    javascript_jquery常用类

--- TL_Function.js
+++ TL_Function.js
@@ -99,3 +99,23 @@ function TL_obtain_Checkbox_Value(name){
     value = value.substr(0,value.length-1);
     return value;
 }
+
+/**
+ * 判断是web还是pc
+ */
+function TL_Web_Or_Pc(web_url,pc_url) {
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+    if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) ){
+        window.location.href=web_url;
+    }else{
+        window.location.href=pc_url;
+    }
+}
